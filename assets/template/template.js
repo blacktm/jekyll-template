12139dd6e6643a6b93d00893eca5dc4346dca3af
Tom Black <blacktm@gmail.com>
2015-08-03 01:52:49 -0400

    Fixing FastClick issue

--- assets/template/template.js
+++ assets/template/template.js
@@ -33,4 +33,4 @@ window.addEventListener('resize', function (event) {
 
 
 // Attach FastClick to mobile menu buttons
-FastClick.attach(document.getElementById('site-header'));
+FastClick.attach(document.getElementById('site-nav-mobile'));
